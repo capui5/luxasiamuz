a0e0e067c14174610d977cadb0bd99234e6e82b1
Tejasri Bonala <tejasri.bonala@aarini.com>
2023-11-07 10:52:12 +0000

    first commit

--- luxasia/webapp/controller/mainmenu.controller.js
+++ luxasia/webapp/controller/mainmenu.controller.js
@@ -11,13 +11,101 @@ sap.ui.define([
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("customersearch");
         },
-        createprofile: function () {
+        transaction: function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("newcustomer");
+            oRouter.navTo("transaction");
         },
         contact: function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.navTo("calllist");
+        },
+        onCustPress: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("brandselection");
+        },
+
+        onOpenDialog: function () {
+            this.pDialog ??= this.loadFragment({
+                name: "luxasia.view.newprofile"
+            });
+            this.pDialog.then(function (dialog) {
+                dialog.open();
+            });
+        },
+        
+        onCloseDialog: function () {
+            if (this.pDialog) {
+                this.pDialog.then(function (dialog) {
+                    dialog.close();
+                    dialog.destroy();
+                });
+                this.pDialog = null;
+            }
+        },
+        
+
+        onOpenFrag: function () {
+            this.pDialog ??= this.loadFragment({
+                name: "luxasia.view.cashcarry"
+            });
+            this.pDialog.then(function (dialog) {
+                dialog.open();
+            });
+        },
+
+        onCloseFrag: function () {
+            if (this.pDialog) {
+                this.pDialog.then(function (dialog) {
+                    dialog.close();
+                    dialog.destroy();
+                });
+                this.pDialog = null;
+            }
+        },
+
+        onSearchProduct: function () {
+            console.log("hello world");
+            this.oDialog ??= this.loadFragment({
+                name: "luxasia.view.searchproduct"
+            });
+        
+            this.oDialog.then((oDialog) => oDialog.open());
+        },
+        handleClose : function(){
+            if (this.pDialog) {
+                this.pDialog.then(function (dialog) {
+                    dialog.close();
+                    dialog.destroy();
+                });
+                this.pDialog = null;
+            }
+        },
+    
+        // onOpenFragment: function () {
+        //     // Load the plain fragment and open it as a dialog
+        //     if (!this._oPlainDialog) {
+        //        this._oPlainDialog = new sap.m.Dialog({
+        //           content: sap.ui.xmlfragment("luxasia.view.test", this),
+                
+        //        });
+        //     }
+   
+        //     this.getView().addDependent(this._oPlainDialog);
+   
+        //     this._oPlainDialog.open();
+        //  },
+         onQrPress: function () {
+            this.oDialog ??= this.loadFragment({
+                name: "luxasia.view.qr"
+            });
+            this.oDialog.then(function (dialog) {
+                dialog.open();
+            });
+        },
+        createprofile: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("newcustomer");
         }
+        
     });
 });
